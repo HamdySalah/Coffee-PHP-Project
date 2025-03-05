d975b55c79a6a6920adcda8cb55badda401d27e1
Hamdy Salah <hamdysalah.dev@gmail.com>
2025-03-06 01:08:34 +0200

    update footer and header

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -10,24 +9,18 @@
     <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Great+Vibes" rel="stylesheet">
 
-    <link rel="stylesheet" href="../assets/css/open-iconic-bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/css/css/animate.css">
-    
-    <link rel="stylesheet" href="../assets/css/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="../assets/css/css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="../assets/css/css/magnific-popup.css">
-
-    <link rel="stylesheet" href="../assets/css/css/aos.css">
-
-    <link rel="stylesheet" href="../assets/css/css/ionicons.min.css">
-
-    <link rel="stylesheet" href="../assets/css/css/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="../assets/css/css/jquery.timepicker.css">
-
-    
-    <link rel="stylesheet" href="../assets/css/css/flaticon.css">
-    <link rel="stylesheet" href="../assets/css/css/icomoon.css">
-    <link rel="stylesheet" href="../assets/css/css/style.css">
+    <link rel="stylesheet" href="assets/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/aos.css">
+    <link rel="stylesheet" href="assets/css/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="assets/css/jquery.timepicker.css">
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+    <link rel="stylesheet" href="assets/css/icomoon.css">
+    <link rel="stylesheet" href="assets/css/style.css">
   </head>
   <body>
   	<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
@@ -38,11 +31,10 @@
 	      </button>
 	      <div class="collapse navbar-collapse" id="ftco-nav">
 	        <ul class="navbar-nav ml-auto">
-	          <li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
+	          <li class="nav-item active"><a href="index.html" class="nav-link" >Home</a></li>
 	          <li class="nav-item"><a href="menu.html" class="nav-link">Menu</a></li>
 	          <li class="nav-item"><a href="services.html" class="nav-link">Services</a></li>
 	          <li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
-	         
 	          <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
 	          <li class="nav-item cart"><a href="cart.html" class="nav-link"><span class="icon icon-shopping_cart"></span></a>
 			  <li class="nav-item"><a href="login.html" class="nav-link">login</a></li>
