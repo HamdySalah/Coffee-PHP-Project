855c85859ec4590701d560b657300c105f464834
Hamdy Salah <hamdysalah.dev@gmail.com>
2025-03-06 01:10:07 +0200

    update footer and header

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php require "includes/header.php"; ?>
 <?php 
+echo "</br>";
+echo "</br>";
+echo "</br>";
+echo "</br>";
+echo "</br>";
+echo "</br>";
+echo "</br>";
+echo "</br>";
 echo "<h1>Home</h1>";
 echo "<p>Welcome to the home page.</p>";
 ?>
