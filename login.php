3ed54f86eed60eca2dd0b7eed59ea31e84f111fb
Hamdy Salah <hamdysalah.dev@gmail.com>
2025-03-06 04:25:44 +0200

    update reg & login forms

--- login.php
+++ login.php
@@ -1,7 +1,61 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Coffee - Free Bootstrap 4 Template by Colorlib</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Great+Vibes" rel="stylesheet">
 
+    <link rel="stylesheet" href="assets/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/aos.css">
+    <link rel="stylesheet" href="assets/css/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="assets/css/jquery.timepicker.css">
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+    <link rel="stylesheet" href="assets/css/icomoon.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+<section class="ftco-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 ftco-animate">
+			<form action="#" class="billing-form ftco-bg-dark p-3 p-md-5">
+				<h3 class="mb-4 billing-heading">Login</h3>
+	          	<div class="row align-items-end">
+	          		<div class="col-md-12">
+	                <div class="form-group">
+	                	<label for="Email">Email</label>
+	                  <input type="text" class="form-control" name=""email placeholder="Email">
+	                </div>
+	              </div>
+                 
+	              <div class="col-md-12">
+	                <div class="form-group">
+	                	<label for="Password">Password</label>
+	                    <input type="password" class="form-control" name="logpass" placeholder="Password">
+	                </div>
 
-echo "Hello omar";
+                </div>
+                <div class="col-md-12">
+                	<div class="form-group mt-4">
+							<div class="radio">
+                                <button class="btn btn-primary py-3 px-4">Login</button>
+						    </div>
+					</div>
+                </div>
 
-?>
+               
+	          </form><!-- END -->
+          </div> <!-- .col-md-8 -->
+          </div>
+        </div>
+      </div>
+    </section> <!-- .section -->
 <?php require "includes/footer.php"; ?>
--- register.php
+++ register.php
@@ -9,49 +9,49 @@
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label for="Username">Username</label>
-                                <input type="text" class="form-control" placeholder="Username">
+                                <input type="text" class="form-control" name="username" placeholder="Username">
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label for="Email">Email</label>
-                                <input type="text" class="form-control" placeholder="Email">
+                                <input type="text" class="form-control" name="email" placeholder="Email">
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label for="Password">Password</label>
-                                <input type="password" class="form-control" placeholder="Password">
+                                <input type="password" class="form-control" name="pass" placeholder="Password">
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label for="CPassword">Confirm Password</label>
-                                <input type="password" class="form-control" placeholder="Confirm Password">
+                                <input type="password" class="form-control" name="cpass" placeholder="Confirm Password">
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label for="room">Room No.</label>
-                                <input type="text" class="form-control" placeholder="Room No.">
+                                <input type="text" class="form-control" name="room" placeholder="Room No.">
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label for="ext">EXT</label>
-                                <input type="text" class="form-control" placeholder="EXT">
+                                <input type="text" class="form-control" name="ext" placeholder="EXT">
                             </div>
                         </div>
                         <div class="col-md-12">
                             <div class="form-group">
                                 <label for="pic">Profile Picture</label>
-                                <input type="file" class="form-control">
+                                <input type="file" name="pic" class="form-control">
                             </div>
                         </div>
                         <div class="col-md-12">
                             <div class="form-group mt-4">
-                                <button type="submit" class="btn btn-primary py-3 px-4">Save</button>
-                                <button type="reset" class="btn btn-secondary py-3 px-4">Reset</button>
+                                <button type="submit" name="submit" class="btn btn-primary py-3 px-4">Save</button>
+                                <button type="reset" name="reset" class="btn btn-secondary py-3 px-4">Reset</button>
                             </div>
                         </div>
                     </div>
