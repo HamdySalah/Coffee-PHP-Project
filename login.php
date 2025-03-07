167ba5bbaf72b47f46ae926d045b02f0cc26e3f3
Hamdy Salah <hamdysalah.dev@gmail.com>
2025-03-07 02:10:00 +0200

    update login

--- login.php
+++ login.php
@@ -1,22 +1,14 @@
-<?php require "includes/header.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Coffee - Free Bootstrap 4 Template by Colorlib</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Great+Vibes" rel="stylesheet">
 
-<<<<<<< HEAD
-<?php 
-
-
-?>
-<html>
-<section class="home-slider owl-carousel">
-
-<div class="slider-item" style="background-image: url(assets/images/bg_1.jpg);" data-stellar-background-ratio="0.5">
-    <div class="overlay"></div>
-  <div class="container">
-    <div class="row slider-text justify-content-center align-items-center">
-
-      <div class="col-md-7 col-sm-12 text-center ftco-animate">
-          <h1 class="mb-3 mt-5 bread">Login</h1>
-          <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home</a></span> <span>Login</span></p>
-=======
     <link rel="stylesheet" href="assets/css/open-iconic-bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/animate.css">
     <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
@@ -65,50 +57,6 @@
           </div> <!-- .col-md-8 -->
           </div>
         </div>
->>>>>>> f9c6a60 (updated login)
       </div>
-
-    </div>
-  </div>
-</div>
-</section>
-
-<section class="ftco-section">
-<div class="container">
-  <div class="row">
-    <div class="col-md-12 ftco-animate">
-      <form action="#" class="billing-form ftco-bg-dark p-3 p-md-5">
-          <h3 class="mb-4 billing-heading">Login</h3>
-            <div class="row align-items-end">
-                <div class="col-md-12">
-              <div class="form-group">
-                  <label for="Email">Email</label>
-                <input type="text" class="form-control" placeholder="Email">
-              </div>
-            </div>
-           
-            <div class="col-md-12">
-              <div class="form-group">
-                  <label for="Password">Password</label>
-                  <input type="password" class="form-control" placeholder="Password">
-              </div>
-
-          </div>
-          <div class="col-md-12">
-              <div class="form-group mt-4">
-                      <div class="radio">
-                          <button class="btn btn-primary py-3 px-4">Login</button>
-                      </div>
-              </div>
-          </div>
-
-         
-        </form><!-- END -->
-    </div> <!-- .col-md-8 -->
-    </div>
-  </div>
-</div>
-</section> <!-- .section -->
-
-</html>
+    </section> <!-- .section -->
 <?php require "includes/footer.php"; ?>
