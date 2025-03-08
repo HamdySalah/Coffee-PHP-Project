d6a54bd8e5ebc47e133799c5813138141a411df7
YasmeenHosam <yasmeenhosam.yh@gmail.com>
2025-03-08 04:48:41 +0200

    yasmeen commit

--- config.php
+++ config.php
@@ -9,10 +9,17 @@ class Database {
 
 
     // omar confg
-    private $host = "localhost";
+    /*private $host = "localhost";
     private $dbname = "cafeteria_db";
     private $username = "admin";
     private $password = "123"; 
+    private $conn;*/
+
+    //yasmeen confg
+    private $host = "localhost";
+    private $dbname = "cafeteria";
+    private $username = "root";
+    private $password = ""; 
     private $conn;
 
     public function connect() {
