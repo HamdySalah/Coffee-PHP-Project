28db2656a02b0c64ce6ffa80c9b648895dad57a8
OmarMahmoud2002 <omarmahmoud.devv@gmail.com>
2025-03-10 04:23:23 +0200

    update defult photo

--- user.php
+++ user.php
@@ -42,21 +42,6 @@ $base_path = '/php_project/'; // Set to '' if at web root (e.g., /var/www/html/)
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
-    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,700" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Great+Vibes" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/open-iconic-bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/animate.css">
-    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="assets/css/magnific-popup.css">
-    <link rel="stylesheet" href="assets/css/aos.css">
-    <link rel="stylesheet" href="assets/css/ionicons.min.css">
-    <link rel="stylesheet" href="assets/css/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="assets/css/jquery.timepicker.css">
-    <link rel="stylesheet" href="assets/css/flaticon.css">
-    <link rel="stylesheet" href="assets/css/icomoon.css">
-    <link rel="stylesheet" href="assets/css/style.css">
     <style>
         .user-card {
             background: #1a1a1a;
@@ -89,8 +74,8 @@ $base_path = '/php_project/'; // Set to '' if at web root (e.g., /var/www/html/)
         }
     </style>
 </head>
+<?php require "includes/header.php"; ?>
 <body>
-    <?php require "includes/header.php"; ?>
 
     <section class="ftco-section">
         <div class="container">
@@ -110,8 +95,7 @@ $base_path = '/php_project/'; // Set to '' if at web root (e.g., /var/www/html/)
                                         if ($profile_picture && file_exists($absolute_path)): ?>
                                             <img src="<?php echo htmlspecialchars($base_path . $profile_picture); ?>" alt="<?php echo htmlspecialchars($user['user_name']); ?>">
                                         <?php else: ?>
-                                            <img src="assets/images/default-user.jpg" alt="Default User">
-                                            <p class="debug">Profile Picture: <?php echo htmlspecialchars($profile_picture ?: 'None'); ?> - Exists: <?php echo file_exists($absolute_path) ? 'Yes' : 'No'; ?></p>
+                                            <img src="uploads/users/img_avatar.png" alt="Default User">
                                         <?php endif; ?>
                                         <div class="card-body">
                                             <h5 class="card-title"><?php echo htmlspecialchars($user['user_name']); ?></h5>
@@ -136,20 +120,5 @@ $base_path = '/php_project/'; // Set to '' if at web root (e.g., /var/www/html/)
 
     <?php require "includes/footer.php"; ?>
 
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/jquery-migrate-3.0.1.min.js"></script>
-    <script src="assets/js/popper.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/jquery.easing.1.3.js"></script>
-    <script src="assets/js/jquery.waypoints.min.js"></script>
-    <script src="assets/js/jquery.stellar.min.js"></script>
-    <script src="assets/js/owl.carousel.min.js"></script>
-    <script src="assets/js/jquery.magnific-popup.min.js"></script>
-    <script src="assets/js/aos.js"></script>
-    <script src="assets/js/jquery.animateNumber.min.js"></script>
-    <script src="assets/js/bootstrap-datepicker.js"></script>
-    <script src="assets/js/jquery.timepicker.min.js"></script>
-    <script src="assets/js/scrollax.min.js"></script>
-    <script src="assets/js/main.js"></script>
 </body>
 </html>
